7f666089d754cd3a27543fc1973d85086738f43a
Benno Straub <benno.straub@outlook.de>
2017-07-26 13:08:56 +0200

    Added perspective header

---
+++ include/stx/math/perspective.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "mat4.hpp"
+#include "vec2.hpp"
+
+namespace stx {
+
+mat4 perspective(float fov, float width, float height, float zNear, float zFar) {
+#ifdef xassert
+	xassert(zNear < zFar);
+	xassert(zNear == zNear);
+	xassert(zFar == zFar);
+	xassert(fov > 0);
+	xassert(fov == fov);
+#endif
+
+	float const h = cosf(0.5f * fov) / sin(0.5f * fov);
+	float const w = h * height / width;
+
+	mat4 result = mat4::zero();
+	result[0][0] = w;
+	result[1][1] = h;
+	result[2][3] = -1;
+
+	result[2][2] = -(zFar + zNear) / (zFar - zNear);
+	result[3][2] = -(2 * zFar * zNear) / (zFar - zNear);
+
+	// mProjection[2][2] = zFar / (zNear - zFar);
+	// mProjection[3][2] = -(zFar * zNear) / (zFar - zNear);
+
+	return result;
+}
+
+mat4 orthographic(vec2 const& size, float zNear, float zFar) {
+#ifdef xassert
+	xassert(zNear < zFar);
+	xassert(zNear == zNear && zFar == zFar);
+	xassert(size.x != 0 && size.y != 0);
+	xassert(size.x == size.x && size.y == size.y);
+#endif // defined(xassert)
+
+	mat4 result = mat4().scale(vec3(1 / size.x, 1 / size.y, 2 / (zFar - zNear))).translate(vec3(0, 0, -zNear));
+
+	return result;
+}
+
+} // namespace stx
