9a6946baf0e66c718bdfb260c2912786a7628456
Benno Straub <benno.straub@outlook.de>
2017-08-20 16:37:54 +0200

    Improved update.sh

--- update.sh
+++ update.sh
@@ -2,15 +2,24 @@
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-[ ! -d "${scriptdir}/include/xmath/" ] && mkdir "${scriptdir}/include/xmath/"
-
-rm "${scriptdir}/include/xmath/"*
-
-for header in "${scriptdir}/include/stx/math/"*
-do
-	if [ ! -d "${header}" ]
-	then
-		name=`basename ${header%.*}`
-		printf '#include "%s"' "../stx/math/${name}.hpp" > "${scriptdir}/include/xmath/${name}"
+function relink() {
+	if [ -z "$3" ]; then
+		if [ -d "${scriptdir:?}/$2" ]; then
+			rm -Rf   "${scriptdir:?}/$2"
+		fi
 	fi
-done
+
+	mkdir -p "${scriptdir}/$2"
+
+	for header in "${scriptdir}/$1/"*.hpp
+	do
+		if [ ! -d "${header}" ]
+		then
+			name=`basename ${header%.*}`
+			outfile="${scriptdir}/$2/$3${name}"
+			printf '#include "%s"' "$(realpath --relative-to="$(dirname "${outfile}")" "${header}")" > "${outfile}"
+		fi
+	done
+}
+
+relink "include/stx/math" "include/xmath"
